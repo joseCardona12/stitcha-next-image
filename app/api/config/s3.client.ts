5a54eb002ba09247958382475120d2fac448309a
user <user@192.168.1.13>
2026-01-21 14:33:48 -0500

    fix load envs

--- app/api/config/s3.client.ts
+++ app/api/config/s3.client.ts
@@ -2,5 +2,9 @@ import { S3Client } from "@aws-sdk/client-s3";
 import { ENVS } from "./getEnv";
 
 export const S3: S3Client = new S3Client({
-  region: ENVS.AWS_REGION,
+  region: ENVS.REGION,
+  credentials: {
+    accessKeyId: ENVS.ACCESS_KEY,
+    secretAccessKey: ENVS.SECRET_ACCESS,
+  },
 });
--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     API_KEY: process.env.API_KEY,
     REGION: process.env.REGION,
     ACCESS_KEY_ID: process.env.ACCESS_KEY_ID,
-    SECRET_ACCESS: process.env.SECRET_ACCESS,
+    SECRET_ACCESS_KEY: process.env.SECRET_ACCESS_KEY,
   },
 };
 
